48a9a4776d8f2db3c2d20e458ebe455086125e01
Loan Kim Nguyen <loan.nguyen2311883@hcmut.edu.vn>
2026-05-04 00:29:23 +0700

    change idle time of db

--- share-bill-backend-v2/internal/db/connection.go
+++ share-bill-backend-v2/internal/db/connection.go
@@ -17,8 +17,8 @@ func NewDatabase(dbSource string) *pgxpool.Pool {
 	config.MaxConns = 20                       
 	config.MinConns = 2                        
 	config.MaxConnLifetime = time.Hour         
-	config.MaxConnIdleTime = 30 * time.Minute  
-
+	config.MaxConnIdleTime = 3 * time.Minute  
+	config.HealthCheckPeriod = 1 * time.Minute 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	pool, err := pgxpool.NewWithConfig(ctx, config)
